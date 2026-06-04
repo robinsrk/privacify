97f1fd85c14b29775e9baa52b24af1087d73fbd0
Robin <robinsrk3@gmail.com>
2026-06-04 16:19:35 +0600

    chore(pre-release): bump versio to 1.5.1 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.robin.privacify"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5.1-pre"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
